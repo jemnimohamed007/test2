9578309e9751ff8de33d8727e944fac0a69aac21
Mohamed Jemni <79259510+jemnimohamed007@users.noreply.github.com>
2024-01-11 19:22:41 +0100

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,17 +1,24 @@
-from tkinter import *
-from tkinter.ttk import *
-from time import strftime
+from selenium import webdriver
+import time
 
-root = Tk()
-root.title("Clock")
+url = "https://faucetearner.org/faucet.php"
+button_xpath = '//*[@id="body"]/div[1]/div[3]/div[2]/div/div[4]/div[1]/form/button'
 
-def time():
-    string = strftime("%H:%M:%S %p")
-    label.config(text = string)
-    label.after(1000, time)
+# Set up the webdriver (make sure to download the appropriate driver for your browser)
+driver = webdriver.Chrome()
 
-label = Label(root, font =("valentime.ttf.otf", 80), background = "black", foreground = "cyan")
-label.pack(anchor="center")
-time()
+# Open the website
+driver.get(url)
 
-root.mainloop()
+# Function to click the button every minute
+def click_button_every_minute():
+    while True:
+        # Find the button and click it
+        button = driver.find_element_by_xpath(button_xpath)
+        button.click()
+
+        # Wait for one minute
+        time.sleep(70)
+
+# Call the function
+click_button_every_minute()
